c400ea69d5d39ff5ff2dbf40b19cf048a68f5cbd
salomonhippie <salomonhippie@gmail.com>
2008-08-12 10:49:21 +0000

    now with deltatime

--- ParticleSystem/OrientationModifier.h
+++ ParticleSystem/OrientationModifier.h
@@ -16,7 +16,7 @@ public:
 		 ParticleGroup<T>* particleGroup) {
       T* particles = particleGroup->GetParticles();
       for (unsigned int i=0; i<particleGroup->GetNumberOfActiveParticles(); i++) {
-          particles[i].orientation[2] += (rand()/((float)RAND_MAX))*10;
+          particles[i].orientation[2] += (rand()/((float)RAND_MAX))*deltaTime;
       }
       
     }
